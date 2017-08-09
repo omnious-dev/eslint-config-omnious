78a961c83abbc227e31f5f775a26bcf5be914637
NoahLuftYang <ykd1217@gmail.com>
2017-08-09 11:43:54 +0900

    Add rules for jsdoc

--- index.js
+++ index.js
@@ -16,21 +16,26 @@ module.exports = {
      */
     // Disallow the use of console
     'no-console': 'warn',
+    'valid-jsdoc': 'error',
 
     /*
      * Best Practices
      */
     // Enforce consistent brace style for all control statements
-    'curly': ['error', 'multi-or-nest', 'consistent'],
+    curly: ['error', 'multi-or-nest', 'consistent'],
     // Require the use of === and !==
-    'eqeqeq': ['error', 'always', { null: 'ignore' }],
-    // Require the use of let or const
-    'no-var': ['error'],
+    eqeqeq: ['error', 'always', { null: 'ignore' }],
+
+    /*
+     * ECMAScript 6
+     */
+    // Require let or const instead of var
+    'no-var': 'error',
 
     /*
      * Strict Mode
      */
     // Require or disallow strict mode directives
-    'strict': 'error'
+    strict: 'error'
   }
 };
